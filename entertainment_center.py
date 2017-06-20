521dced035d67a57c7f1eb635ad61b6cdaddc361
narotamsingh <narotam.singh@gmail.com>
2017-06-20 17:57:21 +0530

    line

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,39 +1,57 @@
 import media
 import fresh_tomatoes
 """
-This Create multiple instances of Python Class (Movie from media) to represent favorite movies;
-group all the instances together in a list. Then called a function open_movies_page that
-takes in one argument, which is a list of movies, and creates an HTML file which will
-display all of favorite movies.
+This Create multiple instances of Python Class (Movie from media) to represent
+favorite movies; group all the instances together in a list. Then called a
+function open_movies_page that takes in a argument, which is a list of movies,
+and creates an HTML file which will display all of favorite movies.
 """
-noor = media.Movie("Noor",
-                        "Noor is an Indian comedy-drama film that features Sonakshi Sinha in the lead titular role.",
-                        "https://upload.wikimedia.org/wikipedia/en/c/c1/Noor_Poster.jpg",
-                        "https://www.youtube.com/watch?v=DHuM6C6EyXE")
-llb_story = media.Movie("Jolly LLB 2",
-                        "Indian black comedy film, A courtroom drama which satirizes the notion of the Indian legal system.",
-                        "https://upload.wikimedia.org/wikipedia/en/4/4b/Jolly_LLB_2_first_look.jpg",
-                        "https://www.youtube.com/watch?v=-T4ESR1aGUo")
-ok_jaanu = media.Movie("Ok Jaanu",
-                        "Indian romantic drama film, a young couple in a live-in relationship in Mumbai with an older couple.",
-                        "https://upload.wikimedia.org/wikipedia/en/b/b8/Ok_Jaanu_poster.jpg",
-                        "https://www.youtube.com/watch?v=eNTAj6J1mcU")
-kaabil = media.Movie("Kaabil",
-                        "Indian Hindi-language action thriller film. It features a love affair between two blind people.",
-                        "https://upload.wikimedia.org/wikipedia/en/c/ce/Kaabil_Movie_Poster.jpg",
-                        "https://www.youtube.com/watch?v=O65JHeSDhuc")
-half_girlfriend = media.Movie("Half Girlfriend",
-                        "Half Girlfriend is an Indian romantic drama film based on the novel of the same name written by Chetan Bhagat.",
-                        "https://upload.wikimedia.org/wikipedia/en/6/6e/Half_Girlfriend_Poster.jpg",
-                        "https://www.youtube.com/watch?v=KmlBnmyelHI")
-badrinath = media.Movie("Badrinath Ki Dulhania",
-                        "Indian romantic comedy film, marks the second installment of a franchise that began with Humpty Sharma Ki Dulhania.",
-                        "https://upload.wikimedia.org/wikipedia/en/7/76/Badrinath_Ki_Dulhania_Cover.jpg",
-                        "https://www.youtube.com/watch?v=1YBl3Zbt80A")
+
+
+noor = media.Movie(
+    "Noor",
+    "Noor is an Indian comedy-drama film that features Sonakshi\
+    Sinha in the lead titular role.",
+    "https://upload.wikimedia.org/wikipedia/en/c/c1/Noor_Poster.jpg",
+    "https://www.youtube.com/watch?v=DHuM6C6EyXE")
+
+llb = media.Movie(
+    "Jolly LLB 2",
+    "Indian black comedy film, A courtroom drama which\
+    satirizes the notion of the Indian legal system.",
+    "https://upload.wikimedia.org/wikipedia/en/4/4b/Jolly_LLB_2_first_look.jpg",
+    "https://www.youtube.com/watch?v=-T4ESR1aGUo")
+ok = media.Movie(
+    "Ok Jaanu",
+    "Indian romantic drama film, a young couple in a \
+    live-in relationship in Mumbai with an older couple.",
+    "https://upload.wikimedia.org/wikipedia/en/b/b8/Ok_Jaanu_poster.jpg",
+    "https://www.youtube.com/watch?v=eNTAj6J1mcU")
+kaabil = media.Movie(
+    "Kaabil",
+    "Indian Hindi-language action thriller film. It features\
+    a love affair between two blind people.",
+    "https://upload.wikimedia.org/wikipedia/en/c/ce/Kaabil_Movie_Poster.jpg",
+    "https://www.youtube.com/watch?v=O65JHeSDhuc")
+half_gf = media.Movie(
+    "Half Girlfriend",
+    "Half Girlfriend is an Indian romantic drama film based\
+    on the novel of same name written by Chetan Bhagat.",
+    "https://upload.wikimedia.org/wikipedia/en/6/6e/Half_Girlfriend_Poster.jpg",
+    "https://www.youtube.com/watch?v=KmlBnmyelHI")
+badrinath = media.Movie(
+    "Badrinath Ki Dulhania",
+    "Indian romantic comedy film, marks the second\
+    installment of a franchise that began with Humpty \
+    Sharma Ki Dulhania.",
+    "https://upload.wikimedia.org/wikipedia/en/7/76/Badrinath_Ki_Dulhania_Cover.jpg",
+    "https://www.youtube.com/watch?v=1YBl3Zbt80A")
 
 # Group all the instances together in a list
-movies = [noor, llb_story, ok_jaanu, kaabil, half_girlfriend, badrinath]
+movies = [noor, llb, ok, kaabil, half_gf, badrinath]
 
-# Function called open_movies_page from starter code repository that contains a Python module called fresh_tomatoes.py
-# that takes in one argument, which is a list of movies, and creates an HTML file which will display all of favorite movies.
+# Function called open_movies_page from starter code repository that contains
+# a Python module called fresh_tomatoes.py that takes in one argument, which
+# is a list of movies, and creates an HTML file which will display all of
+# favorite movies.
 fresh_tomatoes.open_movies_page(movies)
--- media.py
+++ media.py
@@ -1,12 +1,16 @@
 import webbrowser
 """
-This is a Data structure (i.e. a Python Class) to store favorite movies, including movie title,
-story line, poster URL and a YouTube link to the movie trailer.
+This is a Data structure (i.e. a Python Class) to store favorite movies,
+including movie title, story line, poster URL and a YouTube link to the
+movie trailer.
 """
 
+
 class Movie():
+    """Constructor to create instances of movie"""
     #  Constructor for the movie class to create instances of movie
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
